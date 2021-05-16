436d3aa39b31f93d0511f0d956031311ec60ea0b
Divakar2430 <divakarmuthu1994@gmail.com>
2021-05-16 18:49:29 +0530

    Added new project

---
+++ src/org/test/hexaware/login/CourseDetails.java
@@ -0,0 +1,17 @@
+package org.test.hexaware.login;
+
+	public class CourseDetails {
+	public void javaCourse() {
+		 System.out.println("java course");
+	}
+	public void oracleCourse(){
+		System.out.println("Oracle course");
+	}
+	public static void main(String[] args) {
+		CourseDetails details = new CourseDetails();
+		details.oracleCourse();
+		details.javaCourse();
+	}
+	}
+
+
